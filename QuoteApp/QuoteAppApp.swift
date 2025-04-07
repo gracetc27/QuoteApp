5134ff093eab54af2effcacef87ed61041835f09
Grace Couch <gracetcouch@gmail.com>
2025-04-07 13:39:11 +0100

    initial commit

---
+++ QuoteApp/QuoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteAppApp.swift
+//  QuoteApp
+//
+//  Created by Grace couch on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuoteAppTests/QuoteAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteAppTests.swift
+//  QuoteAppTests
+//
+//  Created by Grace couch on 07/04/2025.
+//
+
+import Testing
+@testable import QuoteApp
+
+struct QuoteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
